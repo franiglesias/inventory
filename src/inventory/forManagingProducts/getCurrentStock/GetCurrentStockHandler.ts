069684128f02c22b9ea14a3f1a703360c1b68976
Fran Iglesias <franiglesias@mac.com>
2025-04-28 17:10:25 +0200

    Added preliminary collection of products in the Handler

--- src/inventory/forManagingProducts/getCurrentStock/GetCurrentStockHandler.ts
+++ src/inventory/forManagingProducts/getCurrentStock/GetCurrentStockHandler.ts
@@ -2,6 +2,17 @@ import {GetCurrentStock} from './GetCurrentStock'
 import {GetCurrentStockResponse} from './GetCurrentStockResponse'
 
 export class GetCurrentStockHandler {
+    private products: Map<string, object> = new Map()
+
+    constructor() {
+        this.products = new Map()
+        this.products.set('no-stock-product-id', {
+            id: 'no-stock-product-id',
+            name: 'No Stock Product',
+            stock: 0,
+        })
+    }
+
     handle(query: GetCurrentStock): GetCurrentStockResponse {
         if (!this.hasStockOf(query.productId)) {
             return GetCurrentStockResponse.withError(`Product Id ${query.productId} exhausted`)
