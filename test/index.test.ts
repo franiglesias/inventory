2334fc7aaa27046e9aabd3eb564b5035d5b70546
Fran Iglesias <franiglesias@mac.com>
2025-04-28 08:44:46 +0200

    First test and tentative implementation of needed objects

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,41 @@
-import {describe} from 'vitest'
+import {describe, expect, it} from 'vitest'
 
-describe('Inventory System', () => {
+class GetCurrentStock {
+    public productId: string
+    constructor(productId: string) {
+        this.productId = productId
+    }
+}
 
+class GetCurrentStockResponse {
+}
+
+class GetCurrentStockHandler {
+    handle(query: GetCurrentStock):GetCurrentStockResponse {
+        throw UnknownProduct.withId(query.productId);
+    }
+}
+
+class UnknownProduct implements Error {
+    message: string
+    name: string
+
+    constructor(name: string, message: string) {
+        this.name = name
+        this.message = message
+    }
+
+    static withId(productId: string) {
+        return new UnknownProduct('UnknownProduct', `Product Id ${productId} doesn't exist`)
+    }
+}
+
+describe('For Managing Products Port', () => {
+    describe('When we ask the current stock of a not existing product', () => {
+        it('Should fail with Unknown Product Error', () => {
+            const query = new GetCurrentStock('no-existing-product-id')
+            const handler = new GetCurrentStockHandler()
+            expect(() => handler.handle(query)).toThrow(UnknownProduct)
+        })
+    })
 })
